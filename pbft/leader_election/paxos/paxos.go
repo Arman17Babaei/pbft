48a6c929ab4084f7edf510cb109bb0b4876d1ae6
Zichun-Cai <zcaiao@connect.ust.hk>
2025-07-24 21:56:25 +0800

    update Paxos Strategy

--- pbft/leader_election/paxos/paxos.go
+++ pbft/leader_election/paxos/paxos.go
@@ -4,6 +4,11 @@ package paxos
 
 import "github.com/Arman17Babaei/pbft/pbft/configs"
 
+// Paxos Election Strategy (Through 1 Paxos Instance):
+// 1. First timeout node broadcast Being-Leader Request
+// 2. Acceptor return vote for Being-Leader Request
+// 3. Receiving majority's vote ([m/2] + 1),
+
 type Node interface {
 	GetLastStableCheckPoint()
 }
@@ -14,7 +19,6 @@ type PaxosElection struct {
 }
 
 func NewPaxosElection(config *configs.Config, node Node) *PaxosElection {
-
 	return &PaxosElection{
 		config: config,
 		node:   node,
